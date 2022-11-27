9fde8361c3a963c0e5aa851fc026bbab89dd96ab
Dimitar Janevski <dimitar.j27@gmail.com>
2022-11-27 10:28:34 -0700

    finish first blog page

--- client/src/components/BlogCard.js
+++ client/src/components/BlogCard.js
@@ -1,10 +1,39 @@
 import React from 'react'
+import { styled } from '@mui/system';
+import { Link, useNavigate } from "react-router-dom";
+
+const Container = styled('div')({
+    boxShadow: "rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;",
+    width:"100%",
+    height:"125px",
+    boxSizing:"border-box",
+    borderRadius:"12px",
+    display:"flex",
+    padding:"25px",
+    alignItems:"center"
+})
+
+const Title = styled('Div')({
+    fontFamily: "Montserrat, sans-serif",
+    color:"#0F1020",
+    fontWeight:"800",
+    fontSize:"24px"
+})
 
 const BlogCard = (props) => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        const name = props.content["name"].toLowerCase().replaceAll(" ","-");
+        navigate(`/blogs/${name}`)
+    }
+
     return (
-        <div>
-            {props.content["name"]}
-        </div>
+        <Container onClick={handleClick}>
+            <Title>
+                {props.content["name"]}
+            </Title>
+        </Container>
     )
 }
 
--- client/src/pages/Blogs.js
+++ client/src/pages/Blogs.js
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
 import NavBar from '../components/NavBar'
 import { Link, useNavigate } from "react-router-dom";
 import { styled } from '@mui/system';
 import Button from "@mui/material/Button";
 import BlogCard from '../components/BlogCard';
+import Dialog from '@mui/material/Dialog';
+import { DialogTitle } from "@mui/material";
+import TextField from '@mui/material/TextField';
 
-const Container = styled('div')({
-    width:"100%",
+const Wrapper = styled('div')({
+    width:"100ww",
+    display:"flex",
+    flexDirection:"column",
+    paddingLeft:"20%",
+    paddingRight:"20%",
+    position:"relative"
 })
 
 const Header = styled('div')({
     display:"flex",
     alignItems:"center",
-    justifyContent:"space-around",
+    justifyContent:"space-between",
     paddingTop:"40px"
 })
 
@@ -27,7 +35,15 @@ const Table = styled('div')({
     alignItems:"center",
     justifyContent:"space-around",
     paddingTop:"40px",
-    flexDirection:"column"
+    flexDirection:"column",
+    gap:"25px"
+})
+
+const Form = styled('div')({
+    display:"flex",
+    flexDirection:"column",
+    gap:"20px",
+    padding:"0 20px 20px 20px"
 })
 
 const blogs = [
@@ -43,22 +59,92 @@ const blogs = [
 ]
 
 const Blogs = () => {
+    const [openDialog, setOpenDialog] = useState(false);
+    const [newTitle, setNewTitle] = useState("");
+    const [newContent, setNewContent] = useState("");
+    const [newPhoto, setNewPhoto] = useState("");
+
+    const handleSubmit = () => {
+        const data = {
+            "title": newTitle,
+            "content": newContent,
+            "photo": newPhoto
+        }
+        console.log(data);
+        setNewTitle("");
+        setNewContent("");
+        setNewPhoto("");
+        setOpenDialog(false);
+    }
+
+    const handleChange = (event) => {
+        if (event.target.id == "title"){
+            setNewTitle(event.target.value);
+        }
+        if (event.target.id == "content"){
+            setNewContent(event.target.value);
+        }
+        if (event.target.id == "photo"){
+            setNewPhoto(event.target.value);
+        }
+    }
+
+    const renderDialog = () => {
+        return(
+            <Dialog 
+                open={openDialog}
+                onClose={()=>{setOpenDialog(false)}}
+                fullWidth
+                maxWidth={"md"}
+            >
+                <DialogTitle>
+                    Post New Blog
+                </DialogTitle>
+                <Form>
+                    <TextField
+                        required
+                        id="title"
+                        label="Title"
+                        value={newTitle}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        required
+                        id="content"
+                        label="Content"
+                        multiline
+                        value={newContent}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        required
+                        id="photo"
+                        label="Photo URL"
+                        value={newPhoto}
+                        onChange={handleChange}
+                    />
+                    <Button variant="contained" color="primary" sx={{color:"#ffffff"}} onClick={handleSubmit}>Post Blog</Button>
+                </Form>
+            </Dialog>
+        )
+    }
     return (
         <div>
             <NavBar></NavBar>
-            <Container>
+            <Wrapper>
                 <Header>
                     <Title>
                         Blogs
                     </Title>
-                    <Button variant="contained" color="primary" sx={{color:"#ffffff"}}>Post Blog</Button>
+                    <Button variant="contained" color="primary" sx={{color:"#ffffff"}} onClick={()=>{setOpenDialog(true)}}>Post Blog</Button>
                 </Header>
                 <Table>
                     {blogs.map((blog) => 
                         <BlogCard content={blog}></BlogCard>
                     )}
                 </Table>
-            </Container>
+            </Wrapper>
+            {renderDialog()}
         </div>
     )
 }
