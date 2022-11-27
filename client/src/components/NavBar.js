b5fd4d4abfb3551834ef209983256fb94577c59d
Dimitar Janevski <dimitar.j27@gmail.com>
2022-11-26 23:31:29 -0700

    add navbar + minor home page changes

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { styled } from "@mui/system";
+import Button from "@mui/material/Button";
+import Logo from "../assets/logo.png"
+
+const Container = styled("div")({
+  display: "flex",
+  width: "100%",
+  justifyContent: "space-around",
+  paddingTop: "20px",
+  position: "absolute",
+});
+
+const NavButton = styled(Button)({
+  color: "#0F1020",
+  backgroundColor: "#F0F8FF",
+  transition: "0.25s",
+  "&:hover": {
+    backgroundColor: "#F0F8FF",
+    textDecoration: "underline",
+    transform: "scale(1.025)",
+  },
+});
+
+const TextContainer = styled('div')({
+    display:"flex",
+    gap:"20px"
+})
+
+const Icon = styled('img')({
+    "&:hover": {
+        cursor:"pointer"
+      },
+})
+
+const NavBar = (props) => {
+  const navigate = useNavigate();
+  const [loggedIn, setLoggedIn] = useState(false);
+  if (loggedIn) {
+    return (
+      <Container>
+        <Icon src={Logo} width="50px" onClick={() => navigate("/")}></Icon>
+        <TextContainer>
+            <NavButton variant="text" onClick={() => navigate("/services")}>
+            Services
+            </NavButton>
+            <NavButton variant="text" onClick={() => navigate("/blogs")}>
+            Blogs
+            </NavButton>
+            <NavButton variant="text" onClick={() => navigate("/marketplace")}>
+            Marketplace
+            </NavButton>
+        </TextContainer>
+      </Container>
+    );
+  } else {
+    return (
+      <Container>
+          <Icon src={Logo} width="50px" onClick={() => navigate("/")}></Icon>
+          <TextContainer>
+                <NavButton variant="text" onClick={() => navigate("/register")}>
+                Create Account
+                </NavButton>
+                <NavButton variant="text" onClick={() => navigate("/login")}>
+                Log In
+                </NavButton>
+          </TextContainer>
+      </Container>
+    );
+  }
+};
+export default NavBar;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,34 +1,13 @@
 import React, { useState} from 'react';
 import { Link, useNavigate } from "react-router-dom";
 import { styled } from '@mui/system';
-import Button from '@mui/material/Button';
-import { Typography } from '@mui/material';
+import NavBar from "../components/NavBar.js"
 import Logo from '../assets/logo.png'
+
 const Page = styled('div')({
   width:"100vw",
   height:"100vh",
   backgroundColor:"#F0F8FF"
-})
-
-const NavBar = styled('div')({
-  display:"flex",
-  width:"90%",
-  justifyContent:"flex-end",
-  gap: "50px",
-  marginRight:"10px",
-  paddingTop:"20px",
-  position:"absolute",
-});
-
-const NavButton = styled(Button)({
-  color: "#0F1020",
-  backgroundColor:"#F0F8FF",
-  transition:"0.25s",
-  "&:hover": {
-    backgroundColor:"#F0F8FF",
-    textDecoration:"underline",
-    transform:"scale(1.05)"
-  }
 })
 
 const Container = styled('div')({
@@ -59,15 +38,13 @@ const SubTitle = styled('div')({
   fontSize:"24px"
 })
 
+
 const Home = () => {
   const navigate = useNavigate();
 
   return(
     <Page>
-      <NavBar>
-        <NavButton variant="text" onClick={() => navigate("/register")}>Create Account</NavButton>
-        <NavButton variant="text">Log In</NavButton>
-      </NavBar>
+      <NavBar></NavBar>
       <Container>
         <img src={Logo} width="300px" height="300px"></img>
         <TextContainer>
