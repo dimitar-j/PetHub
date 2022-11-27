7951479beef3d00b09b6d537a920a028de000ee9
Dimitar Janevski <dimitar.j27@gmail.com>
2022-11-26 23:35:32 -0700

    create other page tamplates

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import './App.css'
 import { BrowserRouter, Route, Routes, } from "react-router-dom";
 import Register from "./pages/Register"
 import Home from "./pages/Home"
+import Blogs from './pages/Blogs';
+import Marketplace from './pages/Marketplace';
+import Services from './pages/Services';
+import LogIn from './pages/LogIn'
 
 function App() {
   return (
@@ -10,6 +14,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/register" element={<Register/>}/>
+          <Route path="/login" element={<LogIn/>}/>
+          <Route path="/blogs" element={<Blogs/>}/>
+          <Route path="/marketplace" element={<Marketplace/>}/>
+          <Route path="/services" element={<Services/>}/>
         </Routes>
       </BrowserRouter>
     </div>
