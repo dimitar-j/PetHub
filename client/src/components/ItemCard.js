de9019e816b4dc32cab09347fdbe1e7b6ecdb0d1
Dimitar Janevski <dimitar.j27@gmail.com>
2022-11-29 09:58:13 -0700

    finish marketplace page

---
+++ client/src/components/ItemCard.js
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import { styled } from "@mui/system";
+import { Link, useNavigate } from "react-router-dom";
+import DialogTitle from "@mui/material/DialogTitle";
+import Dialog from "@mui/material/Dialog";
+
+const Container = styled("div")({
+  boxShadow:
+    "rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;",
+  width: "100%",
+  height: "125px",
+  boxSizing: "border-box",
+  borderRadius: "12px",
+  display: "flex",
+  flexDirection: "column",
+  padding: "25px",
+  alignItems: "flex-start",
+  justifyContent: "center",
+  gap: "5px",
+  transition: "0.1s",
+  "&:hover": {
+    cursor: "pointer",
+    transform: "scale(1.01)",
+  },
+});
+
+const Title = styled("div")({
+  fontFamily: "Montserrat, sans-serif",
+  color: "#0F1020",
+  fontWeight: "800",
+  fontSize: "24px",
+});
+
+const Body = styled("div")({
+  fontFamily: "Montserrat, sans-serif",
+  color: "#0F1020",
+  fontWeight: "400",
+  fontSize: "18px",
+});
+
+const Subtitle = styled("div")({
+  fontFamily: "Montserrat, sans-serif",
+  color: "rgba(0,0,0,0.5)",
+  fontWeight: "100",
+  fontSize: "16px",
+});
+
+const CategoryText = styled("div")({
+  fontFamily: "Montserrat, sans-serif",
+  color: "black",
+  fontWeight: "100",
+  fontSize: "16px",
+  fontStyle: "italic",
+});
+
+const SubContainer = styled("div")({
+  display: "flex",
+  gap: "15px",
+});
+
+const Content = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  padding: "0 25px 50px 25px",
+  gap: "20px",
+});
+
+const ItemCard = (props) => {
+  const navigate = useNavigate();
+  const [openDialog, setOpenDialog] = useState(false);
+
+  const handleClick = () => {
+    setOpenDialog(true);
+  };
+
+  const renderDialog = () => {
+    return (
+      <Dialog open={openDialog} onClose={() => setOpenDialog(false)}>
+        <DialogTitle>
+          <Title>{props.content["title"]}</Title>
+        </DialogTitle>
+        <Content>
+          <Body>${props.content["price"]}</Body>
+          <Body>
+            <a href={`mailto: ${props.content["sellerEmail"]}`}>
+              {props.content["sellerEmail"]}
+            </a>
+          </Body>
+          <Body>{props.content["description"]}</Body>
+        </Content>
+      </Dialog>
+    );
+  };
+
+  return (
+    <>
+      <Container onClick={handleClick}>
+        <Title>{props.content["title"]}</Title>
+        <SubContainer>
+          <Subtitle>${props.content["price"].toFixed(2)}</Subtitle>
+          <CategoryText>{props.content["category"]}</CategoryText>
+        </SubContainer>
+      </Container>
+      {renderDialog()}
+    </>
+  );
+};
+
+export default ItemCard;
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,15 +2,15 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { styled } from "@mui/system";
 import Button from "@mui/material/Button";
-import Logo from "../assets/logo.png"
+import Logo from "../assets/logo.png";
 
 const Container = styled("div")({
   display: "flex",
   width: "100%",
   justifyContent: "space-around",
   paddingTop: "20px",
-  backgroundColor:"#F0F8FF",
-  paddingBottom:"20px"
+  backgroundColor: "#F0F8FF",
+  paddingBottom: "20px",
 });
 
 const NavButton = styled(Button)({
@@ -24,49 +24,49 @@ const NavButton = styled(Button)({
   },
 });
 
-const TextContainer = styled('div')({
-    display:"flex",
-    gap:"20px"
-})
+const TextContainer = styled("div")({
+  display: "flex",
+  gap: "20px",
+});
 
-const Icon = styled('img')({
-    "&:hover": {
-        cursor:"pointer"
-      },
-})
+const Icon = styled("img")({
+  "&:hover": {
+    cursor: "pointer",
+  },
+});
 
 const NavBar = (props) => {
   const navigate = useNavigate();
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(true);
   if (loggedIn) {
     return (
       <Container>
         <Icon src={Logo} width="50px" onClick={() => navigate("/")}></Icon>
         <TextContainer>
-            <NavButton variant="text" onClick={() => navigate("/services")}>
+          <NavButton variant="text" onClick={() => navigate("/services")}>
             Services
-            </NavButton>
-            <NavButton variant="text" onClick={() => navigate("/blogs")}>
+          </NavButton>
+          <NavButton variant="text" onClick={() => navigate("/blogs")}>
             Blogs
-            </NavButton>
-            <NavButton variant="text" onClick={() => navigate("/marketplace")}>
+          </NavButton>
+          <NavButton variant="text" onClick={() => navigate("/marketplace")}>
             Marketplace
-            </NavButton>
+          </NavButton>
         </TextContainer>
       </Container>
     );
   } else {
     return (
       <Container>
-          <Icon src={Logo} width="50px" onClick={() => navigate("/")}></Icon>
-          <TextContainer>
-                <NavButton variant="text" onClick={() => navigate("/register")}>
-                Create Account
-                </NavButton>
-                <NavButton variant="text" onClick={() => navigate("/login")}>
-                Log In
-                </NavButton>
-          </TextContainer>
+        <Icon src={Logo} width="50px" onClick={() => navigate("/")}></Icon>
+        <TextContainer>
+          <NavButton variant="text" onClick={() => navigate("/register")}>
+            Create Account
+          </NavButton>
+          <NavButton variant="text" onClick={() => navigate("/login")}>
+            Log In
+          </NavButton>
+        </TextContainer>
       </Container>
     );
   }
